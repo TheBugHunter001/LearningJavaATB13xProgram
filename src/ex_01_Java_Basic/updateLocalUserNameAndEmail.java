9e94f783cff2c98a9e9b0ebbc19a0aaf16a544c9
Arnab Saikia <arnab2502@gmail.com>
2025-06-10 19:52:02 +0530

    Updated local user name and email

---
+++ src/ex_01_Java_Basic/updateLocalUserNameAndEmail.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class updateLocalUserNameAndEmail {
+    public static void main(String[] args) {
+
+        System.out.println("Updated -local user.name \"Arnab Saikia\" and user.email arnab2502@gmail.com\n");
+
+    }
+}
