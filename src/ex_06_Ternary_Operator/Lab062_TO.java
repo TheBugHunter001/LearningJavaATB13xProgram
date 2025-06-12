dae7aa778b3b1f3d2f28bf78092dc54d3638ca0a
Arnab Saikia <arnab2502@gmail.com>
2025-06-12 19:45:48 +0530

    12th Jun's Task

---
+++ src/ex_06_Ternary_Operator/Lab062_TO.java
@@ -0,0 +1,12 @@
+package ex_06_Ternary_Operator;
+
+public class Lab062_TO {
+
+    public static void main(String[] args) {
+
+        // Age to vote?
+        int age = 10;
+        String canIVote = age > 18 ? "Yes" : "No";
+        System.out.println(canIVote);
+    }
+}
