6c935988a21d1df0523e00d9d2b83604cff0ee57
Arnab Saikia <arnab2502@gmail.com>
2025-06-19 07:52:02 +0530

    Test 19th Jun- Arithmetic_operations

---
+++ src/Test_19th_Jun/Arithmetic_operations.java
@@ -0,0 +1,15 @@
+package Test_19th_Jun;
+
+public class Arithmetic_operations {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        float z = 3.2f;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%z);
+    }
+}
