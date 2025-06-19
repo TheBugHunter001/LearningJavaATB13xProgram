126820dffa61d18f35e6b842a852b6d27f279a59
Arnab Saikia <arnab2502@gmail.com>
2025-06-19 08:03:31 +0530

    Test 19th Jun- Comparison_with_logical_operators

---
+++ src/Test_19th_Jun/Comparison_with_logical_operators.java
@@ -0,0 +1,16 @@
+package Test_19th_Jun;
+
+public class Comparison_with_logical_operators {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        System.out.println(a==b); // Should be false as a is not equal to b
+        System.out.println(a!=b); // Should be true
+        System.out.println(a<b); // Should be true as a is less than b
+        System.out.println(a>b); // Should be false as a is less than b
+        System.out.println(a<=b); // Should be true as a is less than b
+        System.out.println(a>=b); // Should be false as a is less than b
+
+    }
+}
