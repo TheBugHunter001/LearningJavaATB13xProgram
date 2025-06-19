67bdb0e3c810a8c884fca7a4ab71f50c4914e575
Arnab Saikia <arnab2502@gmail.com>
2025-06-19 07:45:52 +0530

    Test 19th Jun- Variable_declaration

---
+++ src/Test_19th_Jun/Variable_declaration.java
@@ -0,0 +1,15 @@
+package Test_19th_Jun;
+
+public class Variable_declaration {
+    public static void main(String[] args) {
+        int a = 4;
+        double b = 44.89;
+        boolean c = true;
+        char d = 'a';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
