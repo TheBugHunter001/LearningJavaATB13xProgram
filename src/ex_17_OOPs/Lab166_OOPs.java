27970978c69beb2e76bd82282e3567d5d18f49f4
Arnab Saikia <arnab2502@gmail.com>
2025-06-30 19:19:32 +0530

    30th Jun -OOPS basic

---
+++ src/ex_17_OOPs/Lab166_OOPs.java
@@ -0,0 +1,13 @@
+package ex_17_OOPs;
+
+public class Lab166_OOPs {
+    public static void main(String[] args) {
+        Person amit = new Person();
+
+
+
+        Person pramod = new Person();
+
+
+    }
+}
