f285a0bc95f448ed7b5b024592983b943aff6e06
Arnab Saikia <arnab2502@gmail.com>
2025-07-06 19:18:36 +0530

    3rd Jul Tasks

--- src/ex_Java_Tasks_03rd_Jul/MethodOverloading/AddTwoNumbers.java
+++ src/ex_Java_Tasks_03rd_Jul/MethodOverloading/AddTwoNumbers.java
@@ -3,7 +3,7 @@ package ex_Java_Tasks_03rd_Jul.MethodOverloading;
 public class AddTwoNumbers {
     public static void main(String[] args) {
         Calculator c1 = new Calculator();
-        System.out.println(c1.add(5,6));
+        System.out.println(c1.add(5,7));
         System.out.println(c1.add(4.7,9.2));
     }
 
